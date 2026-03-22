e86899b5a335fca1ff834f41514575b313858c02
bsp2 <bs@tkscript.de>
2026-03-22 18:43:47 +0100

    merge from tks repo

--- shadervg/PointsRoundAA14_2Conic.h
+++ shadervg/PointsRoundAA14_2Conic.h
@@ -1,8 +1,8 @@
 // ----
-// ---- file   : LineStripFlatAA14_2Radial.h
+// ---- file   : PointsRoundAA14_2Conic.h
 // ---- author : Bastian Spiegel <bs@tkscript.de>
 // ---- legal  : Distributed under terms of the MIT license (https://opensource.org/licenses/MIT)
-// ----          Copyright 2014-2026 by bsp
+// ----          Copyright 2026 by bsp
 // ----
 // ----          Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 // ----          associated documentation files (the "Software"), to deal in the Software without restriction, including
@@ -24,85 +24,92 @@
 // ----
 // ----
 
-class LineStripFlatAA14_2Radial : public ShaderVG_Shape {
+class PointsRoundAA14_2Conic : public ShaderVG_Shape {
 
   public:
    // ------------ vertex shader --------------
    const char *vs_src =
       "uniform mat4  u_transform; \n"
-      "uniform float u_stroke_w; \n"
+      "uniform float u_point_radius; \n"
       "uniform vec2  u_paint_start; \n"
+      "uniform vec2  u_paint_scale; \n"
       " \n"
-      "ATTRIBUTE vec2  a_vertex; \n"
-      "ATTRIBUTE vec2  a_vertex_n; \n"
+      "ATTRIBUTE vec2 a_vertex; \n"
       " \n"
       "VARYING_OUT vec2 v_vertex_mp; \n"
-      "flat VARYING_OUT vec2 v_plane_n; \n"
       "VARYING_OUT vec2 v_paint_pos; \n"
       " \n"
       "void main(void) { \n"
-      "  vec2 v1 = a_vertex * 0.25; \n"
-      "  vec2 v2 = a_vertex_n * 0.25; \n"
-      "  vec2 vN = normalize(v2 - v1); \n"
-      "  vec2 vD = vN * u_stroke_w; \n"
-      "  vec2 v1L = vec2(v1.x + vD.y, v1.y - vD.x); \n"
-      "  vec2 v2L = vec2(v2.x + vD.y, v2.y - vD.x); \n"
-      "  vec2 v1R = vec2(v1.x - vD.y, v1.y + vD.x); \n"
-      "  vec2 v2R = vec2(v2.x - vD.y, v2.y + vD.x); \n"
+      "  vec2 vCtr = a_vertex * 0.25; \n"
       "  vec2 v; \n"
       " \n"
       "  float index = float(gl_VertexID); \n"
       " \n"
       "  if(index > 4.9) { \n"
-      "    v = v1R; \n"
+      "    v = vec2(vCtr.x - u_point_radius, vCtr.y + u_point_radius); \n"  // LB
       "  } \n"
       "  else if(index > 3.9) { \n"
-      "    v = v2R; \n"
+      "    v = vCtr + vec2(u_point_radius, u_point_radius); \n"  // RB
       "  } \n"
       "  else if(index > 2.9) { \n"
-      "    v = v1L; \n"
+      "    v = vCtr - vec2(u_point_radius, u_point_radius); \n"  // LT
       "  } \n"
       "  else if(index > 1.9) { \n"
-      "    v = v2R; \n"
+      "    v = vCtr + vec2(u_point_radius, u_point_radius); \n"  // RB
       "  } \n"
       "  else if(index > 0.9) { \n"
-      "    v = v2L; \n"
+      "    v = vec2(vCtr.x + u_point_radius, vCtr.y - u_point_radius); \n"  // RT
       "  } \n"
       "  else { \n"
-      "    v = v1L; \n"
+      "    v = vCtr - vec2(u_point_radius, u_point_radius); \n"  // LT
       "  } \n"
       " \n"
       "  gl_Position = u_transform * vec4(v,0,1); \n"
-      "  v_vertex_mp = v - v1; \n"
-      "  v_plane_n   = vec2(vN.y, -vN.x); \n"
-      "  v_paint_pos = (v - u_paint_start); \n"
+      "  v_vertex_mp = v - vCtr; \n"
+      "  v_paint_pos = (v - u_paint_start) * u_paint_scale; \n"
       "} \n"
       ;
 
    // ------------ fragment shader ------------
    const char *fs_src =
-      "uniform vec4      u_color_stroke; \n"
-      "uniform float     u_stroke_w; \n"
-      "uniform float     u_aa_range; \n"
+      "uniform vec4  u_color_stroke; \n"
+      "uniform float u_point_radius; \n"
+      "uniform float u_aa_range; \n"
 #ifdef SHADERVG_DEBUG_FRAG
-      "uniform float     u_debug; \n"
+      "uniform float u_debug; \n"
 #endif // SHADERVG_DEBUG_FRAG
       "uniform sampler2D u_paint_tex; \n"
-      "uniform float     u_paint_ob_len; \n"
+      "uniform float     u_paint_angle01; \n"
       " \n"
       "VARYING_IN vec2 v_vertex_mp; \n"
-      "flat VARYING_IN vec2 v_plane_n; \n"
       "VARYING_IN vec2 v_paint_pos; \n"
       " \n"
       "void main(void) { \n"
-      "  float d = abs(dot(v_vertex_mp, v_plane_n)); \n"
-      "  float a = 1.0 - smoothstep(u_stroke_w - u_aa_range, u_stroke_w, d); \n"
-      "  float dp = length(v_paint_pos) * u_paint_ob_len; \n"
-      "  vec4 cp = TEXTURE2D(u_paint_tex, vec2(dp, 0.0)); \n"
+      "  vec2 vd = abs(v_vertex_mp); \n"
+      "  float d = length(vd); \n"
+      // aa
+      "  float a = 1.0 - smoothstep(u_point_radius - u_aa_range, u_point_radius, d); \n"
+      "  vec2 n = normalize(abs(v_paint_pos)); \n"
+      "  float ap = atan(n.y / n.x) * (1.0 / 6.2831853071795864); \n"
+      "  if(v_paint_pos.x > 0.0) {\n"
+      "    if(v_paint_pos.y < 0.0) { \n"
+      "      ap = 1.0 - ap; \n"
+      "    } \n"
+      "  } \n"
+      "  else if(v_paint_pos.y < 0.0) { \n"
+      "      ap += 0.5; \n"
+      "  } \n"
+      "  else { \n"
+      "    ap = 0.5 - ap; \n"
+      "  } \n"
+      "  ap += u_paint_angle01; \n"
+      "  if(ap >= 1.0) ap -= 1.0; \n"   // (note) use texture repeat ?
+      "  else if(ap < 0.0) ap += 1.0; \n"
+      "  vec4 cp = TEXTURE2D(u_paint_tex, vec2(ap, 0.0)); \n"
       "  FRAGCOLOR = vec4(u_color_stroke.rgb * cp.rgb, u_color_stroke.a * cp.a * a); \n"
 #ifdef SHADERVG_DEBUG_FRAG
       "  if(u_debug > 0.0) { \n"
-      "    FRAGCOLOR = vec4(u_color_stroke.r, a, u_color_stroke.b, u_color_stroke.a); \n"
+      "    FRAGCOLOR = vec4(1.0, a, a, 1.0); \n"
       "  } \n"
 #endif // SHADERVG_DEBUG_FRAG
       "} \n"
@@ -111,14 +118,13 @@ class LineStripFlatAA14_2Radial : public ShaderVG_Shape {
    sBool validateShapeShader(void) {
       return
             (-1 != shape_a_vertex)
-         && (-1 != shape_a_vertex_n)
          && (-1 != shape_u_transform)
          && (-1 != shape_u_color_stroke)
-         && (-1 != shape_u_stroke_w)
+         && (-1 != shape_u_point_radius)
          && (-1 != shape_u_aa_range)
          && (-1 != shape_u_paint_start)
          && (-1 != shape_u_paint_tex)
-         && (-1 != shape_u_paint_ob_len)
+         && (-1 != shape_u_paint_angle01)
          ;
    }
 
@@ -130,6 +136,5 @@ class LineStripFlatAA14_2Radial : public ShaderVG_Shape {
       return YAC_FALSE;
    }
 
-   // (note) see also: shadervg.cpp:loc_DrawLineStripFlatAAVBOPaint()
-
+   // see also: Shape::drawPointsRoundAAVBO14_2Paint()
 };
--- shadervg/PointsRoundAA14_2PatternDecalAlpha.h
+++ shadervg/PointsRoundAA14_2PatternDecalAlpha.h
@@ -1,8 +1,8 @@
 // ----
-// ---- file   : LineStripFlatAA14_2Radial.h
+// ---- file   : PointsRoundAA14_2PatternDecalAlpha.h
 // ---- author : Bastian Spiegel <bs@tkscript.de>
 // ---- legal  : Distributed under terms of the MIT license (https://opensource.org/licenses/MIT)
-// ----          Copyright 2014-2026 by bsp
+// ----          Copyright 2026 by bsp
 // ----
 // ----          Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 // ----          associated documentation files (the "Software"), to deal in the Software without restriction, including
@@ -24,85 +24,82 @@
 // ----
 // ----
 
-class LineStripFlatAA14_2Radial : public ShaderVG_Shape {
+class PointsRoundAA14_2PatternDecalAlpha : public ShaderVG_Shape {
 
   public:
    // ------------ vertex shader --------------
    const char *vs_src =
       "uniform mat4  u_transform; \n"
-      "uniform float u_stroke_w; \n"
+      "uniform float u_point_radius; \n"
       "uniform vec2  u_paint_start; \n"
+      "uniform vec2  u_paint_ob_size; \n"
+      "uniform float u_paint_ob_len; \n"
       " \n"
-      "ATTRIBUTE vec2  a_vertex; \n"
-      "ATTRIBUTE vec2  a_vertex_n; \n"
+      "ATTRIBUTE vec2 a_vertex; \n"
       " \n"
       "VARYING_OUT vec2 v_vertex_mp; \n"
-      "flat VARYING_OUT vec2 v_plane_n; \n"
-      "VARYING_OUT vec2 v_paint_pos; \n"
+      "VARYING_OUT vec2 v_paint_uv; \n"
       " \n"
       "void main(void) { \n"
-      "  vec2 v1 = a_vertex * 0.25; \n"
-      "  vec2 v2 = a_vertex_n * 0.25; \n"
-      "  vec2 vN = normalize(v2 - v1); \n"
-      "  vec2 vD = vN * u_stroke_w; \n"
-      "  vec2 v1L = vec2(v1.x + vD.y, v1.y - vD.x); \n"
-      "  vec2 v2L = vec2(v2.x + vD.y, v2.y - vD.x); \n"
-      "  vec2 v1R = vec2(v1.x - vD.y, v1.y + vD.x); \n"
-      "  vec2 v2R = vec2(v2.x - vD.y, v2.y + vD.x); \n"
+      "  vec2 vCtr = a_vertex * 0.25; \n"
       "  vec2 v; \n"
       " \n"
       "  float index = float(gl_VertexID); \n"
       " \n"
       "  if(index > 4.9) { \n"
-      "    v = v1R; \n"
+      "    v = vec2(vCtr.x - u_point_radius, vCtr.y + u_point_radius); \n"  // LB
       "  } \n"
       "  else if(index > 3.9) { \n"
-      "    v = v2R; \n"
+      "    v = vCtr + vec2(u_point_radius, u_point_radius); \n"  // RB
       "  } \n"
       "  else if(index > 2.9) { \n"
-      "    v = v1L; \n"
+      "    v = vCtr - vec2(u_point_radius, u_point_radius); \n"  // LT
       "  } \n"
       "  else if(index > 1.9) { \n"
-      "    v = v2R; \n"
+      "    v = vCtr + vec2(u_point_radius, u_point_radius); \n"  // RB
       "  } \n"
       "  else if(index > 0.9) { \n"
-      "    v = v2L; \n"
+      "    v = vec2(vCtr.x + u_point_radius, vCtr.y - u_point_radius); \n"  // RT
       "  } \n"
       "  else { \n"
-      "    v = v1L; \n"
+      "    v = vCtr - vec2(u_point_radius, u_point_radius); \n"  // LT
       "  } \n"
       " \n"
       "  gl_Position = u_transform * vec4(v,0,1); \n"
-      "  v_vertex_mp = v - v1; \n"
-      "  v_plane_n   = vec2(vN.y, -vN.x); \n"
-      "  v_paint_pos = (v - u_paint_start); \n"
+      "  v_vertex_mp = v - vCtr; \n"
+      "  v_paint_uv  = (v - u_paint_start) * u_paint_ob_size * u_paint_ob_len; \n"
       "} \n"
       ;
 
    // ------------ fragment shader ------------
    const char *fs_src =
-      "uniform vec4      u_color_stroke; \n"
-      "uniform float     u_stroke_w; \n"
-      "uniform float     u_aa_range; \n"
+      "uniform vec4  u_color_fill; \n"
+      "uniform vec4  u_color_stroke; \n"
+      "uniform float u_decal_alpha; \n"
+      "uniform float u_point_radius; \n"
+      "uniform float u_aa_range; \n"
 #ifdef SHADERVG_DEBUG_FRAG
-      "uniform float     u_debug; \n"
+      "uniform float u_debug; \n"
 #endif // SHADERVG_DEBUG_FRAG
       "uniform sampler2D u_paint_tex; \n"
-      "uniform float     u_paint_ob_len; \n"
+      "uniform vec2      u_paint_ndir; \n"
       " \n"
       "VARYING_IN vec2 v_vertex_mp; \n"
-      "flat VARYING_IN vec2 v_plane_n; \n"
-      "VARYING_IN vec2 v_paint_pos; \n"
+      "VARYING_IN vec2 v_paint_uv; \n"
       " \n"
       "void main(void) { \n"
-      "  float d = abs(dot(v_vertex_mp, v_plane_n)); \n"
-      "  float a = 1.0 - smoothstep(u_stroke_w - u_aa_range, u_stroke_w, d); \n"
-      "  float dp = length(v_paint_pos) * u_paint_ob_len; \n"
-      "  vec4 cp = TEXTURE2D(u_paint_tex, vec2(dp, 0.0)); \n"
-      "  FRAGCOLOR = vec4(u_color_stroke.rgb * cp.rgb, u_color_stroke.a * cp.a * a); \n"
+      "  vec2 vd = abs(v_vertex_mp); \n"
+      "  float d = length(vd); \n"
+      // aa
+      "  float a = 1.0 - smoothstep(u_point_radius - u_aa_range, u_point_radius, d); \n"
+      "  vec2 uv; \n"
+      "  uv.x = v_paint_uv.x * u_paint_ndir.x - v_paint_uv.y * u_paint_ndir.y; \n"
+      "  uv.y = v_paint_uv.x * u_paint_ndir.y + v_paint_uv.y * u_paint_ndir.x; \n"
+      "  float ap = TEXTURE2D(u_paint_tex, uv).TEX_ALPHA; \n"
+      "  FRAGCOLOR = vec4(mix(u_color_fill.rgb, u_color_stroke.rgb, u_color_stroke.a * ap * u_decal_alpha), u_color_fill.a * a); \n"
 #ifdef SHADERVG_DEBUG_FRAG
       "  if(u_debug > 0.0) { \n"
-      "    FRAGCOLOR = vec4(u_color_stroke.r, a, u_color_stroke.b, u_color_stroke.a); \n"
+      "    FRAGCOLOR = vec4(u_color_fill.r, a, u_color_fill.b, u_color_fill.a); \n"
       "  } \n"
 #endif // SHADERVG_DEBUG_FRAG
       "} \n"
@@ -111,14 +108,17 @@ class LineStripFlatAA14_2Radial : public ShaderVG_Shape {
    sBool validateShapeShader(void) {
       return
             (-1 != shape_a_vertex)
-         && (-1 != shape_a_vertex_n)
          && (-1 != shape_u_transform)
+         && (-1 != shape_u_color_fill)
          && (-1 != shape_u_color_stroke)
-         && (-1 != shape_u_stroke_w)
+         && (-1 != shape_u_decal_alpha)
+         && (-1 != shape_u_point_radius)
          && (-1 != shape_u_aa_range)
          && (-1 != shape_u_paint_start)
-         && (-1 != shape_u_paint_tex)
+         && (-1 != shape_u_paint_ob_size)
          && (-1 != shape_u_paint_ob_len)
+         && (-1 != shape_u_paint_ndir)
+         && (-1 != shape_u_paint_tex)
          ;
    }
 
@@ -130,6 +130,5 @@ class LineStripFlatAA14_2Radial : public ShaderVG_Shape {
       return YAC_FALSE;
    }
 
-   // (note) see also: shadervg.cpp:loc_DrawLineStripFlatAAVBOPaint()
-
+   // see also: Shape::drawPointsRoundAAVBO14_2Paint()
 };
